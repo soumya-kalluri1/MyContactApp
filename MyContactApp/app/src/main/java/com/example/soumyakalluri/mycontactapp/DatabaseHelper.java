b5ae0df0a19746107085db21921af999e6f4dd5b
soumya-kalluri1 <soumya.kalluri1@gmail.com>
2017-05-18 10:59:25 -0700

    Added name, number, and address fields

--- MyContactApp/app/src/main/java/com/example/soumyakalluri/mycontactapp/DatabaseHelper.java
+++ MyContactApp/app/src/main/java/com/example/soumyakalluri/mycontactapp/DatabaseHelper.java
@@ -1,8 +1,11 @@
 package com.example.soumyakalluri.mycontactapp;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 /**
  * Created by Soumya Kalluri on 5/11/2017.
@@ -13,6 +16,13 @@ public class DatabaseHelper extends SQLiteOpenHelper{
     public static final String TABLE_NAME = "contact_table";
     public static final String COL_1 = "ID";
     public static final String COL_2 = "NAME";
+    public static final String COL_3 = "NUMBER";
+    public static final String COL_4 = "ADDRESS";
+
+    public DatabaseHelper (Context context)
+    {
+        super(context, DATABASE_NAME, null, 4);
+    }
 
     public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, DATABASE_NAME, null, version);
@@ -20,11 +30,33 @@ public class DatabaseHelper extends SQLiteOpenHelper{
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL("CREATE TABLE " + TABLE_NAME + " (ID_INTEGER_PRIMARY_KEY_AUTOINCREMENT, NAME TEXT, NUMBER TEXT, ADDRESS TEXt)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
 
+    public boolean insertData(String name, String number, String address)
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2, name);
+        contentValues.put(COL_3, number);
+        contentValues.put(COL_4, address);
+        Log.d("MyContact", "DatabaseHelper: Set up contentValues");
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        if (result == -1) return false;
+        else return true;
+    }
+
+    public Cursor getAllData()
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select * from " + TABLE_NAME, null);
+        return res;
     }
 }
--- MyContactApp/app/src/main/java/com/example/soumyakalluri/mycontactapp/MainActivity.java
+++ MyContactApp/app/src/main/java/com/example/soumyakalluri/mycontactapp/MainActivity.java
@@ -1,13 +1,92 @@
 package com.example.soumyakalluri.mycontactapp;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Context;
+import android.database.Cursor;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    DatabaseHelper myDb;
+    EditText editName;
+    EditText editNumber;
+    EditText editAddress;
+    Button btnAddData;
+    Button viewContacts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myDb = new DatabaseHelper(this);
+
+        //add the layout vars
+        editName = (EditText)findViewById(R.id.editText_name);
+        editNumber = (EditText)findViewById(R.id.editText_number);
+        editAddress = (EditText)findViewById(R.id.editText_address);
+
+        //btnAddData = (Button)findViewById(R.id.button_addData);
+    }
+
+    public void addData(View v)
+    {
+        boolean isInserted = myDb.insertData(editName.getText().toString(),
+                editNumber.getText().toString(),
+                editAddress.getText().toString());
+        Context context = getApplicationContext();
+        CharSequence text;
+
+        if (isInserted == true)
+        {
+            Log.d("MyContact","Success inserting data");
+            //insert toast message here...
+            text = "Data inserted!";
+            Toast toast = Toast.makeText(context, text, Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
+        else
+        {
+            Log.d("MyContact", "Failure inserting data");
+            //insert toast message here...
+            text = "Data couldn't be inserted";
+            Toast toast = Toast.makeText(context, text, Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+
+    public void viewData(View v)
+    {
+        Cursor res = myDb.getAllData();
+        if (res.getCount() == 0)
+        {
+            showMessage("Error","No data is found on the database");
+            //output message using Log.d and Toast
+            return;
+        }
+
+        StringBuffer buffer = new StringBuffer();
+        //set up a loop with the Cursor (res) using moveToNext
+
+            //append each COL to the buffer
+            // display message using ShowMessage
+        showMessage("Data", buffer.toString());
+    }
+
+    private void showMessage(String title, String message)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true); //cancel using back button
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.show();
     }
 }
